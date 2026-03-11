3f6a22fab028488cd0b5e7281d5769359766ec5f
Zimaaaa <103002563+Zimaaaa@users.noreply.github.com>
2026-03-11 03:24:56 +0100

    Delete view.js

--- view.js
+++
@@ -1,7 +0,0 @@
-const img = document.getElementById('bigImage');
-const name = localStorage.getItem('selectedImage');
-
-img.src = 'images/' + name;
-
-let mode = localStorage.getItem('mode');
-localStorage.setItem('mode', mode === 'human' ? 'nature' : 'human');
